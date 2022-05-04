5892323c281e148ac3c05620715f7cb00aac0dd9
WillRosa <willianluisviera@gmail.com>
2022-05-04 19:31:13 -0300

    Auto-generated commit [ci skip]

---
+++ assets/js/meuscript.js
@@ -0,0 +1,7 @@
+// const menuHamburguer = document.getElementById('menuHamburguer');
+
+// function toggleMenu(){
+//     const menuCategorias = document.getElementsByClassName('menuCategorias')
+//     menuCategorias.classList.toggle('active')
+// }
+// menuHamburguer.addEventListener('click', toggleMenu)
