c7477b90836ebde2938da3f94927ef55bc781c7c
WillRosa <willianluisviera@gmail.com>
2022-06-21 20:36:43 -0300

    Auto-generated commit [ci skip]

--- assets/js/meuscript.js
+++ assets/js/meuscript.js
@@ -8,15 +8,3 @@ window.addEventListener("DOMContentLoaded", function(e) {
     }
 
   }, false);
-
-
-  window.addEventListener("DOMContentLoaded", function(e) {
-
-    var stage = document.getElementById("startCondicoes");
-    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
-    var arr = stage.getElementsByTagName("li");
-    for(var i=0; i < arr.length; i++) {
-      arr[i].addEventListener("animationend", fadeComplete, false);
-    }
-
-  }, false);
